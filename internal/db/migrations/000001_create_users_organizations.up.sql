541ef73ba5a7d7dc10bd672d30d81acd52a28be1
Evan Bonfiglio <evanbonfiglio@protonmail.com>
2025-08-31 13:58:29 -0400

    Fix migrations

--- internal/db/migrations/000001_create_users_organizations.up.sql
+++ internal/db/migrations/000001_create_users_organizations.up.sql
@@ -3,9 +3,15 @@ CREATE TABLE organizations (
     name TEXT NOT NULL
 );
 
+CREATE TABLE jobs (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    organization_id BIGINT NOT NULL REFERENCES organizations(id)
+);
+
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    job_id BIGSERIAL REFERENCES jobs(id) ON DELETE CASCADE,
-    organization_id BIGSERIAL REFERENCES organizations(id) ON DELETE SET NULL,
+    job_id BIGINT REFERENCES jobs(id) ON DELETE CASCADE,
+    organization_id BIGINT REFERENCES organizations(id) ON DELETE SET NULL,
 );
--- internal/db/migrations/000002_create_jobs.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE jobs (
-    id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    organization_id BIGSERIAL NOT NULL REFERENCES organizations(id)
-);
-
