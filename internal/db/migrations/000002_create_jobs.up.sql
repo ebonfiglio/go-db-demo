d80d27b479f5886f184c1cc485afc813f497b3f9
Evan Bonfiglio <evanbonfiglio@protonmail.com>
2025-08-31 13:51:17 -0400

    fix migration jobs

--- internal/db/migrations/000002_create_jobs.up.sql
+++ internal/db/migrations/000002_create_jobs.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE jobs (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    organization_id INTEGER NOT NULL REFERENCES organizations(id)
+    organization_id BIGSERIAL NOT NULL REFERENCES organizations(id)
 );
 
